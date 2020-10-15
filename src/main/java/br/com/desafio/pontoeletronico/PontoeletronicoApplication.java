d8e0ee553d373d5344cb925cdf9593898fe607c7
dsamuel32 <diego.samuel.alves@gmail.com>
2020-10-15 15:43:42 -0300

    Comite inicial

---
+++ src/main/java/br/com/desafio/pontoeletronico/PontoeletronicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.pontoeletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoeletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoeletronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/pontoeletronico/PontoeletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.pontoeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoeletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
